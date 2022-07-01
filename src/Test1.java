99039144847e503a07c14bb5bf6044b5e4d4c15c
Admin <Admin@Shashikant>
2022-07-01 21:44:33 +0530

    First Commit!

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "Hello World";
+		
+		System.out.println(s1);
+
+	}
+
+}
