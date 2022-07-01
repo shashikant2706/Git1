0162b1ce18b94887068de69441659a7845a15703
Admin <Admin@Shashikant>
2022-07-01 23:47:59 +0530

    commiting test 2 to branch hello india

---
+++ src/Test2.java
@@ -0,0 +1,14 @@
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "Helo India";
+		
+		System.out.println(s1);
+
+	}
+
+
+}
